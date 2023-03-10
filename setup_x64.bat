4751501a2dd7da3e4512bee49baf496fe4e30ddb
HaruoOgawa <haru.spring.yodai@gmail.com>
2023-03-11 08:23:34 +0900

    環境パスは通さないように修正

--- setup_x64.bat
+++ setup_x64.bat
@@ -5,12 +5,12 @@ set EnvPathRelease=%EnvPath%Release\bin
 set EnvPathDemoRelease=%EnvPath%DemoRelease\bin
 set Path_setup_x64=%EnvPath%setup_x64.bat
 
-setx /M Path "%Path%;%EnvPathDebug%;%EnvPathRelease%;%EnvPathDemoRelease%"
+@rem setx /M Path "%Path%;%EnvPathDebug%;%EnvPathRelease%;%EnvPathDemoRelease%"
 
-attrib +s +h %EnvPathDebug%
-attrib +s +h %EnvPathRelease%
-attrib +s +h %EnvPathDemoRelease%
-attrib +s +h %Path_setup_x64%
+@rem attrib +s +h %EnvPathDebug%
+@rem attrib +s +h %EnvPathRelease%
+@rem attrib +s +h %EnvPathDemoRelease%
+@rem attrib +s +h %Path_setup_x64%
 
 xcopy %EnvPath%Release\Haru86_GraphicsEngine.exe %EnvPath%DemoRelease\ /Y /E /I /H /R
 call %EnvPath%..\compress_tools\squishy-x64.exe -i %EnvPath%DemoRelease\Haru86_GraphicsEngine.exe -o %EnvPath%DemoRelease\Haru86_GraphicsEngine.exe
